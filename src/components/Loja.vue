ffb5e685f5d7a63f9ae129606e0cb51e2045d92e
lleite23 <leandrohenrique@frwk.com.br>
2023-03-15 14:59:48 -0300

    incluindo o conceito de mutation

--- src/components/Loja.vue
+++ src/components/Loja.vue
@@ -31,7 +31,8 @@ export default {
 
             this.sequencia++
          
-            this.$store.state.produtos.push(produto)
+            //this.$store.state.produtos.push(produto)
+            this.$store.commit('adicionarProduto', produto)
         }
     }
 }
